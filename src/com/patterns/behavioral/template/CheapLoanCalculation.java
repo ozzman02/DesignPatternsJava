b096f92099f084a0474e582866a2a6dfc6927c30
oscars <osantamaria@gmail.com>
2021-04-02 23:44:24 -0600

    Template Pattern

---
+++ src/com/patterns/behavioral/template/CheapLoanCalculation.java
@@ -0,0 +1,20 @@
+package com.patterns.behavioral.template;
+
+public class CheapLoanCalculation extends LoanCalculationAlgorithm {
+
+    @Override
+    int getBaseAmount() {
+        return 100000;
+    }
+
+    @Override
+    double getInterest() {
+        return 3;
+    }
+
+    @Override
+    int calculateDiscount() {
+        return 5000;
+    }
+
+}
---
+++ src/com/patterns/behavioral/template/LoanCalculationAlgorithm.java
@@ -0,0 +1,14 @@
+package com.patterns.behavioral.template;
+
+public abstract class LoanCalculationAlgorithm {
+
+    public int calculateLoan() {
+        return (int) (getBaseAmount() * getInterest() - calculateDiscount());
+    }
+
+    abstract int getBaseAmount();
+
+    abstract double getInterest();
+
+    abstract int calculateDiscount();
+}
